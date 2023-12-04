5e83de03939e95f915d21d9cdd196d723d78091a
michael talmor <74512899+surfdude677@users.noreply.github.com>
2023-12-04 19:25:01 +0200

    Add files via upload

---
+++ לומדתמחשבון/לומדתמחשבון/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace לומדתמחשבון
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int n1;
+        int n2;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+          
+        }
+
+       
+        
+
+        private void startclick(object sender, RoutedEventArgs e)
+        {
+            Window2 Sec_window = new Window2();
+            Sec_window.Show();
+            this.Close();
+        }
+    }
+
+   
+}
